18c126afa78192a3b5129ce8f9382d4b9aa515b3
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-21 22:46:55 +0900

    feat: init page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            init: Donas admin page (signed-by: haeram.kim1@gmail.com)
+        </div>
+    );
 }
 
 export default App;

017e65dc343f0259af79c5395cff2717bc392a58
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-26 20:50:19 +0900

    fix: home href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     return (
         <StyledNavbar variant="light" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="/">
                     <img
                         alt="donas-logo"
                         src="/images/logo.png"

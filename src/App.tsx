bdd37a5296edc4985bc9881b9731b6857c5ebdac
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-26 21:24:59 +0900

    feat: use @ alias for webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import Header from './components/Header';
+import LoginPage from '@/pages/LoginPage';
+import Header from '@/components/Header';
 
 const StyledApp = styled.div`
     padding-top: 40px;

ebdb277737185b242964f1c59e0c8922b4fe8b9d
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-26 13:26:20 +0900

    fix: use bootstrap for header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,24 @@
 import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
-const StyledHeader = styled.header`
+const StyledNavbar = styled(Navbar)`
     background-color: #FF3363;
-    position: fixed;
-    width: 100%;
-    padding: 0% 10%;
-    height: 40px;
-    top: 0px;
-`;
-
-const StyledLogo = styled.img`
-    height: 40px;
 `;
 
 export default function Header() {
     return (
-        <StyledHeader>
-            <StyledLogo src="/images/logo.png" alt="donas-logo" />
-        </StyledHeader>
+        <StyledNavbar variant="light" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt="donas-logo"
+                        src="/images/logo.png"
+                        height="25"
+                    />
+                </Navbar.Brand>
+            </Container>
+        </StyledNavbar>
     );
 }
+

4288ff74bbb132c724f413e6731721956c99b50f
haeramkeem <ewqdsacxz2345@gmail.com>
2023-04-02 21:12:01 +0900

    fix: split route pages into external tsx

---
+++ src/pages/Creator.tsx
@@ -0,0 +1,14 @@
+import Card from 'react-bootstrap/Card';
+
+export default function Creator() {
+    return (
+        <>
+            <Card.Body>
+                <Card.Title>Creator Manager</Card.Title>
+                <Card.Text>
+                    TODO: creator manager
+                </Card.Text>
+            </Card.Body>
+        </>
+    );
+}

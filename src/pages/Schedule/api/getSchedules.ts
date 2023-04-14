992052c5119818e89342056479d20f19b7b3c64c
haeramkeem <ewqdsacxz2345@gmail.com>
2023-04-15 02:26:08 +0900

    feat: schedule get dashboard

---
+++ src/pages/Schedule/api/getSchedules.ts
@@ -0,0 +1,22 @@
+import { axios, apiConfig } from '@/utils/axios';
+import { cookieConfig, getCookie } from '@/utils/cookie';
+import { ScheduleInfo } from '@/types';
+
+export type GetSchedulesResponseDTO = {
+    context: {
+        totalPages: number,
+        currentPage: number,
+    }
+    content: Array<ScheduleInfo>,
+};
+
+export function getSchedules(page: string|null): Promise<GetSchedulesResponseDTO> {
+    return axios.get(apiConfig.apis.schedules.httpGET, {
+        headers: {
+            Authorization: getCookie(cookieConfig.names.accessToken),
+        },
+        params: {
+            page: page || "1",
+        },
+    });
+}
---
+++ src/pages/Schedule/components/index.ts
@@ -0,0 +1,2 @@
+export * from './TableData';
+export * from './RegisterScheduleModal';

e4587138fd31e4b6b81c5a3c765e156d783517c5
haeramkeem <ewqdsacxz2345@gmail.com>
2023-04-15 15:15:53 +0900

    feat: register schedule modal layout

--- src/pages/Schedule/components/RegisterScheduleModal.tsx
+++ src/pages/Schedule/components/RegisterScheduleModal.tsx
@@ -1,75 +1,78 @@
+import { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
-// import { MouseEventHandler } from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import * as apiCall from '../api';
+import { MouseEventHandler } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import * as apiCall from '../api';
 // import { input } from '@/utils/getElementById';
+import { CreatorInfoShort } from '@/types';
 
 export function RegisterScheduleModal(props: {show: boolean, handleClose: () => void}) {
-    // const onCancel: MouseEventHandler<HTMLButtonElement> = (e) => {
-    //     e.preventDefault();
-    //     props.handleClose();
-    // };
-    //
-    // const onSubmit: MouseEventHandler<HTMLButtonElement> = (e) => {
-    //     e.preventDefault();
-    //
-    //     apiCall.registerCreator({
-    //         profileImage: (input("register-creator-profile-image").files as FileList)[0],
-    //         name: input("register-creator-name").value,
-    //         youtubeURL: input("register-creator-youtube-url").value,
-    //         twitchURL: input("register-creator-twitch-url").value,
-    //         africaURL: input("register-creator-africa-url").value,
-    //     }).then(() => {
-    //         alert("등록되었습니다.");
-    //     }).catch((err) => {
-    //         alert("등록에 실패하였습니다.");
-    //     }).finally(() => {
-    //         props.handleClose();
-    //     });
-    // };
-    //
-    // return (
-    //     <Modal show={props.show} onHide={props.handleClose}>
-    //         <Modal.Header closeButton>
-    //             <Modal.Title>크리에이터 추가</Modal.Title>
-    //         </Modal.Header>
-    //
-    //         <Form id="register-creator">
-    //             <Modal.Body>
-    //                 <Form.Group className="mb-3" controlId="register-creator-profile-image">
-    //                     <Form.Label>프로필 이미지</Form.Label>
-    //                     <Form.Control type="file" placeholder="" />
-    //                 </Form.Group>
-    //
-    //                 <Form.Group className="mb-3" controlId="register-creator-name">
-    //                     <Form.Label>크리에이터 이름</Form.Label>
-    //                     <Form.Control type="url" placeholder="Creator name" />
-    //                 </Form.Group>
-    //
-    //                 <Form.Group className="mb-3" controlId="register-creator-youtube-url">
-    //                     <Form.Label>유튜브 채널 링크</Form.Label>
-    //                     <Form.Control type="url" placeholder="YouTube URL" />
-    //                 </Form.Group>
-    //
-    //                 <Form.Group className="mb-3" controlId="register-creator-twitch-url">
-    //                     <Form.Label>트위치 방송국 링크</Form.Label>
-    //                     <Form.Control type="url" placeholder="Twitch URL" />
-    //                 </Form.Group>
-    //
-    //                 <Form.Group className="mb-3" controlId="register-creator-africa-url">
-    //                     <Form.Label>아프리카 방송국 링크</Form.Label>
-    //                     <Form.Control type="url" placeholder="Africa URL" />
-    //                 </Form.Group>
-    //             </Modal.Body>
-    //
-    //             <Modal.Footer>
-    //                 <Button variant="outline-danger" onClick={onCancel}>취소</Button>
-    //                 <Button variant="primary" type="submit" onClick={onSubmit}>저장</Button>
-    //             </Modal.Footer>
-    //         </Form>
-    //     </Modal>
-    // );
+    const onCancel: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+        props.handleClose();
+    };
 
-    return <Modal show={props.show} onHide={props.handleClose} />;
+    const onSubmit: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+
+    };
+
+    const [allCreators,setAllCreators] = useState<Array<CreatorInfoShort>>([]);
+    useEffect(() => {
+        apiCall.getAllCreators().then(body => {
+            setAllCreators(body);
+        }).catch((err) => {
+            alert("크리에이터 조회에 실패했습니다.");
+        });
+    }, []);
+
+    return (
+        <Modal show={props.show} onHide={props.handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>스케줄 추가</Modal.Title>
+            </Modal.Header>
+
+            <Form id="register-schedule">
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="register-schedule-creator">
+                        <Form.Label>크리에이터 선택</Form.Label>
+                        <Form.Select>
+                            <option>선택</option>
+                            {allCreators.map((creator, idx) => (
+                                <option value={creator.id}>
+                                    {creator.name} (등록 번호: {creator.id})
+                                </option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="register-schedule-banner-image">
+                        <Form.Label>방송 배너 이미지</Form.Label>
+                        <Form.Control type="file" placeholder="방송 배너 이미지" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="register-schedule-name">
+                        <Form.Label>방송 제목</Form.Label>
+                        <Form.Control type="text" placeholder="방송 제목" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="register-schedule-description">
+                        <Form.Label>방송 상세 설명</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="register-schedule-date">
+                        <Form.Label>방송 시간</Form.Label>
+                        <Form.Control type="datetime-local" placeholder="방송 시간" />
+                    </Form.Group>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="outline-danger" onClick={onCancel}>취소</Button>
+                    <Button variant="primary" type="submit" onClick={onSubmit}>저장</Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    );
 }

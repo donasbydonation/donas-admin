ed892b9ee5fd43c87cad73ba3fd5106433c11b02
haeramkeem <ewqdsacxz2345@gmail.com>
2023-05-26 19:27:03 +0900

    feat: add /admin path prefix for admin page

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="admin">
             <App />
         </BrowserRouter>
     </React.StrictMode>
